2b36796ed9292809d39bcbd2b8dcc448327cb7e4
Rethabile Diale <rethadiale27@gmail.com>
2024-04-20 20:06:05 +0200

    imported initialData

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import { getTasks, createNewTask, patchTask, putTask, deleteTask} from './utils/taskFunctions.js';
 
 // TASK: import initialData
-
+import { initialData } from './initialData.js';
 
 /*************************************************************************************************************************************************
  * FIX BUGS!!!
